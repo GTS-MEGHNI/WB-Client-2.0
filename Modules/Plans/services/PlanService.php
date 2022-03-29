fe0ed8f39161a30c9ae4fd9343ce9d45623cf325
GTS-MEGHNI <meghniucrpg@live.fr>
2022-03-29 13:52:24 +0100

    update programs list

--- Modules/Plans/services/PlanService.php
+++ Modules/Plans/services/PlanService.php
@@ -2,14 +2,19 @@
 
 namespace Modules\Plans\services;
 
+use Illuminate\Support\Facades\DB;
 use Modules\Plans\Entities\ProgramModel;
 
 class PlanService
 {
 
+    /** @noinspection PhpArrayShapeAttributeCanBeAddedInspection */
     public function list(): array
     {
-        return ProgramModel::all()->toArray();
+        return [
+            'list' => ProgramModel::all()->toArray(),
+            'placesLeft' => env('MAX_STUDENTS') - DB::table('classroom.students')->count()
+        ];
     }
 
 }
