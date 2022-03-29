c0619de47d46ab4a2b58867eb824942c62658e81
GTS-MEGHNI <meghniucrpg@live.fr>
2022-03-29 17:55:53 +0100

    fix diaries

--- Modules/Dashboard/Services/DiaryService.php
+++ Modules/Dashboard/Services/DiaryService.php
@@ -45,6 +45,7 @@ class DiaryService
             return $page;
         });
         $diaries = DiaryModel::where(['order_id' => Utility::getUserOngoingSubscription()]);
+        dd($diaries);
         $reach_end = !$diaries->paginate($length)->hasMorePages();
         $diaries->paginate($length);
         if ($diaries->count() == 0)
