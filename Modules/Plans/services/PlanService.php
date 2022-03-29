5e539175b411d388e66342f68776be772d1e1391
GTS-MEGHNI <meghniucrpg@live.fr>
2022-03-29 11:36:57 +0100

    Update plans list

--- Modules/Plans/services/PlanService.php
+++ Modules/Plans/services/PlanService.php
@@ -2,8 +2,6 @@
 
 namespace Modules\Plans\services;
 
-use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\DB;
 use Modules\Plans\Entities\ProgramModel;
 
 class PlanService
@@ -11,10 +9,7 @@ class PlanService
 
     public function list(): array
     {
-        return Arr::collapse([
-            ['list' => ProgramModel::all()->toArray()],
-            ['placesLeft' => env('MAX_STUDENTS') - DB::table('classroom.students')->count()]
-        ]);
+        return ProgramModel::all()->toArray();
     }
 
 }
